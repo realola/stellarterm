014606ede9d9106913cc9d8fcddc74626678c9f0
little-wave <little-wave@lobstr.co>
2019-02-25 19:55:58 +0300

    HistoryRow code simplify to func()

--- src/components/Session/HistoryTableRow.jsx
+++ src/components/Session/HistoryTableRow.jsx
@@ -33,176 +33,15 @@ import PropTypes from 'prop-types';
 import Printify from '../../lib/Printify';
 import AssetCard2 from '../AssetCard2';
 import directory from '../../directory';
+import HistoryRowExternal from './HistoryRowExternal';
 
-export default function HistoryTableRow(props) {
-    const { data, type } = props;
-    let details;
-    let setFlags;
-    let clearFlags;
-    let action;
+export default class HistoryTableRow extends React.Component {
+    getHistorySigner() {
+        const { data } = this.props;
+        const action = data.category.split('_')[1];
 
-    switch (type) {
-        // Case 1 ACCOUNT
-    case 'account':
-        switch (data.category) {
-                // Case 1.1 CREATED
-                // Returns a component with the starting balance and the funding public key.
-
-        case 'account_created':
-            details = {
-                title: 'Account Created',
-                attributes: [
-                    {
-                        header: 'STARTING BALANCE: ',
-                        value: Printify.lightenZeros(data.starting_balance),
-                        isAsset: true,
-                        asset_code: 'XLM',
-                        asset_issuer: null,
-                        domain: 'native',
-                    },
-                    {
-                        header: 'FUNDED BY: ',
-                        value: data.funder,
-                    },
-                ],
-            };
-            break;
-        case 'account_inflation_destination_updated':
-            details = {
-                title: 'Inflation updated',
-                attributes: [
-                    {
-                        header: 'INFLATION DEST: ',
-                        value: data.inflation_dest,
-                    },
-                ],
-            };
-            break;
-
-                // Case 1.2 HOME DOMAIN UPDATED
-                // Returns a compnent with the name of the new home domain.
-        case 'account_home_domain_updated':
-            details = {
-                title: 'Home Domain updated',
-                attributes: [
-                    {
-                        header: 'NEW DOMAIN: ',
-                        value: data.home_domain,
-                    },
-                ],
-            };
-            break;
-
-                // Case 1.3 FLAGS UPDATED
-                // Returns a component listing the updated flags (both set and/or clear).
-        case 'account_flags_updated':
-            setFlags = data.set_flags_s ? data.set_flags_s.map(flag => `set_flag_${flag}`).join(', ') : '';
-            clearFlags = data.clear_flags_s
-                        ? data.clear_flags_s.map(flag => `clear_flag_${flag}`).join(', ')
-                        : '';
-            details = {
-                title: 'Flags updated',
-                attributes: [
-                    {
-                        header: 'FLAGS: ',
-                        value: setFlags + clearFlags,
-                    },
-                ],
-            };
-            break;
-
-                // Case 1.4 THRESHOLDS UPDATED
-                // Returns a component with the updated thresholds
-        case 'account_thresholds_updated':
-            details = {
-                title: 'Thresholds Weights updated',
-                attributes: [
-                    {
-                        header: 'HIGH: ',
-                        value: data.high_threshold,
-                    },
-                    {
-                        header: 'MEDIUM: ',
-                        value: data.med_threshold,
-                    },
-                    {
-                        header: 'LOW: ',
-                        value: data.low_threshold,
-                    },
-                ],
-            };
-            break;
-
-                // Case 1.5, 1.6, and 1.7 SELF SENT, CREDITED and DEBITED
-                // Returns a component with the asset, the amount, and the source/destination
-        default:
-            if (data.to === data.from && data.category === 'account_debited') {
-                details = {
-                    title: 'Sent to self',
-                    attributes: [
-                        {
-                            header: 'AMOUNT: ',
-                            value: Printify.lightenZeros(data.amount),
-                            isAsset: true,
-                            asset_code: data.asset_code,
-                            asset_issuer: data.asset_issuer,
-                            domain: data.asset_code
-                                        ? directory.resolveAssetByAccountId(data.asset_code, data.asset_issuer).domain
-                                        : 'native',
-                        },
-                        {
-                            header: 'TO: ',
-                            value: `${data.to} (self)`,
-                        },
-                        {
-                            header: 'FROM:  ',
-                            value: `${data.from} (self)`,
-                        },
-                    ],
-                };
-            } else {
-                let title;
-                let toFromRow = {};
-                if (data.category === 'account_debited') {
-                    title = 'Sent';
-                    toFromRow = {
-                        header: 'TO: ',
-                        value: data.to,
-                    };
-                } else {
-                    title = 'Received';
-                    toFromRow = {
-                        header: data.category === 'account_debited' ? 'TO:  ' : 'FROM:  ',
-                        value: data.from,
-                    };
-                }
-
-                details = {
-                    title,
-                    attributes: [
-                        {
-                            header: 'AMOUNT: ',
-                            value: Printify.lightenZeros(data.amount),
-                            isAsset: true,
-                            asset_code: data.asset_code,
-                            asset_issuer: data.asset_issuer,
-                            domain: data.asset_code
-                                        ? directory.resolveAssetByAccountId(data.asset_code, data.asset_issuer).domain
-                                        : 'native',
-                        },
-                        toFromRow,
-                    ],
-                };
-            }
-        }
-        break;
-
-        // Case 2 SIGNER
-        // Each has the same relevant data, so they all return the same component.
-    case 'signer':
-        action = data.category.split('_')[1];
         if (data.inflation_dest) {
-            details = {
+            return {
                 title: 'Inflation set',
                 attributes: [
                     {
@@ -211,27 +50,27 @@ export default function HistoryTableRow(props) {
                     },
                 ],
             };
-        } else {
-            details = {
-                title: `Signer ${action}`,
-                attributes: [
-                    {
-                        header: 'KEY WEIGHT: ',
-                        value: data.weight,
-                    },
-                    {
-                        header: 'KEY: ',
-                        value: data.public_key,
-                    },
-                ],
-            };
         }
-        break;
 
-        // Case 3 TRADE
-        // For now, pending and updated offers are not recorded by the Effects SDK, only completed trades.
-    case 'trade':
-        details = {
+        return {
+            title: `Signer ${action}`,
+            attributes: [
+                {
+                    header: 'KEY WEIGHT: ',
+                    value: data.weight,
+                },
+                {
+                    header: 'KEY: ',
+                    value: data.public_key,
+                },
+            ],
+        };
+    }
+
+    getHistoryTrade() {
+        const { data } = this.props;
+
+        return {
             title: 'Traded',
             attributes: [
                 {
@@ -241,8 +80,8 @@ export default function HistoryTableRow(props) {
                     asset_code: data.sold_asset_code,
                     asset_issuer: data.sold_asset_issuer,
                     domain: data.sold_asset_code
-                            ? directory.resolveAssetByAccountId(data.sold_asset_code, data.sold_asset_issuer).domain
-                            : 'native',
+                        ? directory.resolveAssetByAccountId(data.sold_asset_code, data.sold_asset_issuer).domain
+                        : 'native',
                 },
                 {
                     header: 'BOUGHT: ',
@@ -251,17 +90,17 @@ export default function HistoryTableRow(props) {
                     asset_code: data.bought_asset_code,
                     asset_issuer: data.bought_asset_issuer,
                     domain: data.bought_asset_code
-                            ? directory.resolveAssetByAccountId(data.bought_asset_code, data.bought_asset_issuer).domain
-                            : 'native',
+                        ? directory.resolveAssetByAccountId(data.bought_asset_code, data.bought_asset_issuer).domain
+                        : 'native',
                 },
             ],
         };
-        break;
+    }
 
-        // Case 4 Trustline
-        // Each has the same relevant data, so they all return the same component.
-    case 'trustline':
-        details = {
+    getHistoryTrustLine() {
+        const { data } = this.props;
+
+        return {
             title: `Trustline ${data.category.split('_')[1]}`,
             attributes: [
                 {
@@ -278,56 +117,239 @@ export default function HistoryTableRow(props) {
                 },
             ],
         };
-        break;
-    default:
-        break;
     }
 
-    return (
-        <div className="HistoryView__card">
-            <div className="HistoryView__card__container">
-                <h3 className="HistoryView__card__container__title">{details.title}</h3>
-                {details.attributes.map(x => (
-                    <div key={`${x.header}${x.value}`} className="HistoryView__card__line">
-                        <span className="HistoryView__card__container__header">{x.header} </span> {x.value}
-                        {x.isAsset ? (
-                            <span className="HistoryView__asset">
-                                {x.asset_code || 'XLM'}-{x.domain}
-                                <div className="HistoryView__asset__card">
-                                    <AssetCard2 code={x.asset_code || 'XLM'} issuer={x.asset_issuer || null} />
-                                </div>
-                            </span>
-                        ) : (
-                            ''
-                        )}
-                    </div>
-                ))}
-                <div className="HistoryView__external">
-                    View in external website:&nbsp;
-                    <a
-                        href={`https://stellar.expert/explorer/tx/${data.transaction_hash}`}
-                        target="_blank"
-                        rel="nofollow noopener noreferrer">
-                        stellar.expert
-                    </a>
-                    &nbsp;
-                    <a
-                        href={`https://horizon.stellar.org/transactions/${data.transaction_hash}`}
-                        target="_blank"
-                        rel="nofollow noopener noreferrer">
-                        Horizon
-                    </a>
-                </div>
-                <div className="HistoryView__external">
-                    Transaction ID: <strong>{data.transaction_hash}</strong>
+    getAccountHistory() {
+        const { data } = this.props;
+
+        switch (data.category) {
+            // ACCOUNT CREATED
+            // Returns a component with the starting balance and the funding public key.
+        case 'account_created':
+            return this.getCreatedHistory();
+            // INFLATION DESTINATION UPDATED
+            // Returns a component with iflation destination address
+        case 'account_inflation_destination_updated':
+            return this.getInflationUpdateHistory();
+            // HOME DOMAIN UPDATED
+            // Returns a component with the name of the new home domain.
+        case 'account_home_domain_updated':
+            return this.getHomeDomainUpdateHistory();
+            // FLAGS UPDATED
+            // Returns a component listing the updated flags (both set and/or clear).
+        case 'account_flags_updated':
+            return this.getFlagsUpdateHistory();
+            // THRESHOLDS UPDATED
+            // Returns a component with the updated thresholds
+        case 'account_thresholds_updated':
+            return this.getThresholdsUpdateHistory();
+            // SELF SENT, CREDITED and DEBITED
+            // Returns a component with the asset, the amount, and the source/destination
+        default:
+            return this.getTransactionsHistory();
+        }
+    }
+
+    getCreatedHistory() {
+        const { data } = this.props;
+
+        return {
+            title: 'Account Created',
+            attributes: [
+                {
+                    header: 'STARTING BALANCE: ',
+                    value: Printify.lightenZeros(data.starting_balance),
+                    isAsset: true,
+                    asset_code: 'XLM',
+                    asset_issuer: null,
+                    domain: 'native',
+                },
+                {
+                    header: 'FUNDED BY: ',
+                    value: data.funder,
+                },
+            ],
+        };
+    }
+
+    getInflationUpdateHistory() {
+        const { data } = this.props;
+
+        return {
+            title: 'Inflation destination updated',
+            attributes: [
+                {
+                    header: 'INFLATION DEST: ',
+                    value: data.inflation_dest,
+                },
+            ],
+        };
+    }
+
+    getHomeDomainUpdateHistory() {
+        const { data } = this.props;
+
+        return {
+            title: 'Home Domain updated',
+            attributes: [
+                {
+                    header: 'NEW DOMAIN: ',
+                    value: data.home_domain,
+                },
+            ],
+        };
+    }
+
+    getFlagsUpdateHistory() {
+        const { data } = this.props;
+        const setFlags = data.set_flags_s ? data.set_flags_s.map(flag => `set_flag_${flag}`).join(', ') : '';
+        const clearFlags = data.clear_flags_s ? data.clear_flags_s.map(flag => `clear_flag_${flag}`).join(', ') : '';
+
+        return {
+            title: 'Flags updated',
+            attributes: [
+                {
+                    header: 'FLAGS: ',
+                    value: setFlags + clearFlags,
+                },
+            ],
+        };
+    }
+
+    getThresholdsUpdateHistory() {
+        const { data } = this.props;
+
+        return {
+            title: 'Thresholds Weights updated',
+            attributes: [
+                {
+                    header: 'HIGH: ',
+                    value: data.high_threshold,
+                },
+                {
+                    header: 'MEDIUM: ',
+                    value: data.med_threshold,
+                },
+                {
+                    header: 'LOW: ',
+                    value: data.low_threshold,
+                },
+            ],
+        };
+    }
+
+    getTransactionsHistory() {
+        const { data } = this.props;
+        let toFromRow = {};
+        let title;
+
+        if (data.to === data.from && data.category === 'account_debited') {
+            return {
+                title: 'Sent to self',
+                attributes: [
+                    {
+                        header: 'AMOUNT: ',
+                        value: Printify.lightenZeros(data.amount),
+                        isAsset: true,
+                        asset_code: data.asset_code,
+                        asset_issuer: data.asset_issuer,
+                        domain: data.asset_code
+                            ? directory.resolveAssetByAccountId(data.asset_code, data.asset_issuer).domain
+                            : 'native',
+                    },
+                    {
+                        header: 'TO: ',
+                        value: `${data.to} (self)`,
+                    },
+                    {
+                        header: 'FROM:  ',
+                        value: `${data.from} (self)`,
+                    },
+                ],
+            };
+        }
+
+        if (data.category === 'account_debited') {
+            title = 'Sent';
+            toFromRow = {
+                header: 'TO: ',
+                value: data.to,
+            };
+        } else {
+            title = 'Received';
+            toFromRow = {
+                header: data.category === 'account_debited' ? 'TO:  ' : 'FROM:  ',
+                value: data.from,
+            };
+        }
+
+        return {
+            title,
+            attributes: [
+                {
+                    header: 'AMOUNT: ',
+                    value: Printify.lightenZeros(data.amount),
+                    isAsset: true,
+                    asset_code: data.asset_code,
+                    asset_issuer: data.asset_issuer,
+                    domain: data.asset_code
+                        ? directory.resolveAssetByAccountId(data.asset_code, data.asset_issuer).domain
+                        : 'native',
+                },
+                toFromRow,
+            ],
+        };
+    }
+
+    render() {
+        const { data, type } = this.props;
+        let historyRows;
+
+        switch (type) {
+        case 'account':
+            historyRows = this.getAccountHistory();
+            break;
+        case 'signer':
+            historyRows = this.getHistorySigner();
+            break;
+        case 'trade':
+            historyRows = this.getHistoryTrade();
+            break;
+        case 'trustline':
+            historyRows = this.getHistoryTrustLine();
+            break;
+        default:
+            break;
+        }
+
+        const history = historyRows.attributes.map(row => (
+            <div key={`${row.header}${row.value}`} className="HistoryView__card__line">
+                <span className="HistoryView__card__container__header">{row.header} </span> {row.value}
+                {row.isAsset ? (
+                    <span className="HistoryView__asset">
+                        {row.asset_code || 'XLM'}-{row.domain}
+                        <div className="HistoryView__asset__card">
+                            <AssetCard2 code={row.asset_code || 'XLM'} issuer={row.asset_issuer || null} />
+                        </div>
+                    </span>
+                ) : ('')}
+            </div>
+        ));
+
+        return (
+            <div className="HistoryView__card">
+                <div className="HistoryView__card__container">
+                    <h3 className="HistoryView__card__container__title">{historyRows.title}</h3>
+
+                    {history}
+                    <HistoryRowExternal hash={data.transaction_hash} />
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 HistoryTableRow.propTypes = {
     data: PropTypes.instanceOf(Object).isRequired,
-    // How to check data instanceof?
     type: PropTypes.string.isRequired,
 };
