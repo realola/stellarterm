efa030f0169307957f095f90a06a49689c44e6e8
shiny-mountain <egor.l@razortheory.com>
2019-02-26 15:48:03 +0300

    OfferMakerOverview refactor

--- src/components/OfferMakerOverview.jsx
+++ src/components/OfferMakerOverview.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import BigNumber from 'bignumber.js';
 import TrustButton from './Session/TrustButton';
 import Driver from '../lib/Driver';
-import Printify from '../lib/Printify';
 
 export default class OfferMakerOverview extends React.Component {
     static capDigits(input) {
@@ -13,89 +12,62 @@ export default class OfferMakerOverview extends React.Component {
             return input;
         }
     }
-    render() {
+
+    getBalance() {
         const isBuy = this.props.side === 'buy';
-        const login = this.props.d.session.state === 'in';
         const { baseBuying, counterSelling } = this.props.d.orderbook.data;
+        const targetAsset = isBuy ? counterSelling : baseBuying;
+        const maxOffer = this.calculateMaxOffer();
+        const maxOfferView = this.constructor.capDigits(maxOffer);
 
-        const capitalizedSide = isBuy ? 'Buy' : 'Sell';
-        const baseAssetName = baseBuying.getCode();
-        const counterAssetName = counterSelling.getCode();
-
-        let youHave;
-        let insufficientBalanceMessage;
-        const trustNeededAssets = [];
-
-        if (login) {
-            const maxLumenSpend = this.props.d.session.account.maxLumenSpend();
-
-            const baseBalance = baseBuying.isNative() ?
-                maxLumenSpend : this.props.d.session.account.getBalance(baseBuying);
-            const counterBalance = counterSelling.isNative() ?
-                maxLumenSpend : this.props.d.session.account.getBalance(counterSelling);
-
-            if (baseBalance === null) {
-                trustNeededAssets.push(baseBuying);
-            }
-            if (counterBalance === null) {
-                trustNeededAssets.push(counterSelling);
-            }
-
-
-            const targetAsset = isBuy ? counterSelling : baseBuying;
-            const targetBalance = isBuy ? counterBalance : baseBalance;
-
-            const reservedBalance = this.props.d.session.account.getReservedBalance(targetAsset);
-            const maxOffer = (parseFloat(targetBalance) > parseFloat(reservedBalance)) ?
-                targetBalance - reservedBalance : 0;
-            const maxOfferView = Printify.lightenZeros(maxOffer.toString(), 7);
-            youHave = (
+        return (
+            <div>
                 <div className="OfferMaker__youHave">
                     {targetAsset.isNative() ?
                         (<span>You may trade up to {maxOfferView} XLM (due to <a href="#account">
-                         minimum balance requirements</a>.)</span>) :
+                                                               minimum balance requirements</a>.)</span>) :
                         (<span>You have {maxOfferView} {targetAsset.getCode()}</span>)
                     }
                 </div>
-            );
-
-            const inputSpendAmount = isBuy ? this.props.offerState.total : this.props.offerState.amount;
-
-            if (Number(inputSpendAmount) > Number(maxOffer)) {
-                insufficientBalanceMessage = (
+                {this.isInsufficientBalance() &&
                     <p className="OfferMaker__insufficientBalance">
-                      Error: You do not have enough {targetAsset.getCode()} to create this offer.
+                        Error: You do not have enough {targetAsset.getCode()} to create this offer.
                     </p>
-                );
-            }
-        }
-
-        const isButtonReady = this.props.offerState.buttonState === 'ready';
-
-        const submit = login ?
-            (<input
-                type="submit"
-                className="s-button"
-                value={isButtonReady ? `${capitalizedSide} ${baseAssetName}` : 'Creating offer...'}
-                disabled={!this.props.offerState.valid || insufficientBalanceMessage || !isButtonReady} />) :
-            (<span className="OfferMaker__message">
-                <a href="#account">Log in</a> to create an offer
-            </span>);
-
-
-        const summary = this.props.offerState.valid && (
-            <div className="s-alert s-alert--info">
-                {capitalizedSide} {this.props.offerState.amount} {this.constructor.capDigits(baseAssetName)} for{' '}
-                {this.constructor.capDigits(this.props.offerState.total)} {counterAssetName}
+                }
             </div>
         );
+    }
+    getInputSummaryMessage() {
+        const { valid, amount, total } = this.props.offerState;
+        if (!valid) {
+            return null;
+        }
+        const isBuy = this.props.side === 'buy';
+        const capitalizedSide = isBuy ? 'Buy' : 'Sell';
+        const baseAssetName = this.props.d.orderbook.data.baseBuying.getCode();
+        const counterAssetName = this.props.d.orderbook.data.counterSelling.getCode();
 
+        return (
+            <div className="s-alert s-alert--info">
+                {capitalizedSide} {this.constructor.capDigits(amount)} {baseAssetName} for{' '}
+                {this.constructor.capDigits(total)} {counterAssetName}
+            </div>
+        );
+    }
 
-        let error;
-        if (this.props.offerState.errorMessage) {
-            switch (this.props.offerState.errorType) {
+    getResultMessage() {
+        const { successMessage, errorMessage, errorType } = this.props.offerState;
+
+        if (successMessage) {
+            return (
+                <div className="s-alert s-alert--success OfferMaker__message">{successMessage}</div>
+            );
+        }
+
+        if (errorMessage) {
+            switch (errorType) {
             case 'buy_not_authorized':
-                error = (
+                return (
                     <div className="s-alert s-alert--alert OfferMaker__message">
                         Unable to create offer because the issuer has not authorized you to trade this asset. To fix
                         this issue, check with the issuer{"'"}s website.
@@ -104,16 +76,15 @@ export default class OfferMakerOverview extends React.Component {
                         NOTE: Some issuers are restrictive in who they authorize.
                     </div>
                 );
-                break;
             case 'op_low_reserve':
-                error = (
+                return (
                     <div className="s-alert s-alert--alert OfferMaker__message">
                         Your account does not have enough XLM to meet the{' '}
                         <a
                             href="https://www.stellar.org/developers/guides/concepts/fees.html#minimum-account-balance"
                             target="_blank"
                             rel="nofollow noopener noreferrer">
-                          minimum balance
+                            minimum balance
                         </a>
                         . For more info, see <a href="#account">the minimum balance section</a> of the account page.
                         <br />
@@ -123,67 +94,132 @@ export default class OfferMakerOverview extends React.Component {
                             <li>Send at least 1 XLM to your account</li>
                             <li>Cancel an existing an offer</li>
                             <li>
-                                Decrease your minimum balance by <a href="#account/addTrust">unaccepting an asset</a>
+                              Decrease your minimum balance by <a href="#account/addTrust">unaccepting an asset</a>
                             </li>
                         </ul>
                     </div>
                 );
-                break;
             case 'tx_bad_seq':
-                error = (
+                return (
                     <div className="s-alert s-alert--alert OfferMaker__message">
                         Transaction failed because sequence got out of sync. Please reload StellarTerm and try again.
                     </div>
                 );
-                break;
             case 'op_underfunded':
-                error = (
+                return (
                     <div className="s-alert s-alert--alert OfferMaker__message">
                         Transaction failed due to a lack of funds.
                     </div>
                 );
-                break;
             default:
-                error = (
+                return (
                     <div className="s-alert s-alert--alert OfferMaker__message">
                         Failed to create offer.
                         <ul className="OfferMaker__errorList">
-                           <li>Error code: {this.props.offerState.errorType}</li>
+                            <li>Error code: {errorType}</li>
                         </ul>
                     </div>
                 );
             }
         }
+        return null;
+    }
 
-        const success = this.props.offerState.successMessage && (
-            <div className="s-alert s-alert--success OfferMaker__message">{this.props.offerState.successMessage}</div>
-        );
+    getSubmitButton() {
+        const isBuy = this.props.side === 'buy';
+        const capitalizedSide = isBuy ? 'Buy' : 'Sell';
+        const baseAssetName = this.props.d.orderbook.data.baseBuying.getCode();
 
-        const overview = trustNeededAssets.length ?
-            (<div>
-                <p className="OfferMaker__enable">To trade, activate these assets on your account:</p>
-                <div className="row__multipleButtons">
-                    {trustNeededAssets.map(asset => (
-                        <TrustButton
-                            key={`${asset.getCode()}-${asset.getIssuer()}`}
-                            d={this.props.d}
-                            asset={asset}
-                            message={`${asset.getCode()} accepted`}
-                            trustMessage={`Accept ${asset.getCode()}`} />
-                    ))}
-                </div>
-            </div>) :
-            (<div className="OfferMaker__overview">
-                {youHave}
-                {insufficientBalanceMessage}
-                {summary}
-                {error}
-                {success}
-                {submit}
-            </div>);
+        const { valid, buttonState } = this.props.offerState;
+        const isButtonReady = buttonState === 'ready';
+
         return (
-            <div>
-                { overview }
+            <input
+                type="submit"
+                className="s-button"
+                value={isButtonReady ? `${capitalizedSide} ${baseAssetName}` : 'Creating offer...'}
+                disabled={!valid || this.isInsufficientBalance() || !isButtonReady} />
+        );
+    }
+
+    getTrustNeededAssets() {
+        const { baseBuying, counterSelling } = this.props.d.orderbook.data;
+        const { account } = this.props.d.session;
+        const baseBalance = account.getBalance(baseBuying);
+        const counterBalance = account.getBalance(counterSelling);
+
+        const trustNeededAssets = [];
+        if (baseBalance === null) {
+            trustNeededAssets.push(baseBuying);
+        }
+        if (counterBalance === null) {
+            trustNeededAssets.push(counterSelling);
+        }
+        return trustNeededAssets;
+    }
+
+    calculateMaxOffer() {
+        const isBuy = this.props.side === 'buy';
+        const { baseBuying, counterSelling } = this.props.d.orderbook.data;
+        const targetAsset = isBuy ? counterSelling : baseBuying;
+
+        const { account } = this.props.d.session;
+        const maxLumenSpend = account.maxLumenSpend();
+
+        const targetBalance = targetAsset.isNative() ? maxLumenSpend : account.getBalance(targetAsset);
+        const reservedBalance = account.getReservedBalance(targetAsset);
+
+        return (parseFloat(targetBalance) > parseFloat(reservedBalance)) ?
+            targetBalance - reservedBalance : 0;
+    }
+
+    isInsufficientBalance() {
+        const isBuy = this.props.side === 'buy';
+        const maxOffer = this.calculateMaxOffer();
+        const { amount, total } = this.props.offerState;
+
+        return Number(isBuy ? total : amount) > Number(maxOffer);
+    }
+
+    render() {
+        const login = this.props.d.session.state === 'in';
+
+        if (!login) {
+            return (
+                <div>
+                    {this.getInputSummaryMessage()}
+                    <span className="OfferMaker__message">
+                        <a href="#account">Log in</a> to create an offer
+                    </span>
+                </div>
+            );
+        }
+
+        const trustNeededAssets = this.getTrustNeededAssets();
+
+        if (trustNeededAssets.length) {
+            return (
+                <div>
+                    <p className="OfferMaker__enable">To trade, activate these assets on your account:</p>
+                    <div className="row__multipleButtons">
+                        {trustNeededAssets.map(asset => (
+                            <TrustButton
+                                key={`${asset.getCode()}-${asset.getIssuer()}`}
+                                d={this.props.d}
+                                asset={asset}
+                                message={`${asset.getCode()} accepted`}
+                                trustMessage={`Accept ${asset.getCode()}`} />
+                        ))}
+                    </div>
+                </div>
+            );
+        }
+        return (
+            <div className="OfferMaker__overview">
+                {this.getBalance()}
+                {this.getInputSummaryMessage()}
+                {this.getResultMessage()}
+                {this.getSubmitButton()}
             </div>
         );
     }
