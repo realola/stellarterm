3c21b9f3647f2aec184b4fbd259b676995634066
little-wave <little-wave@lobstr.co>
2019-03-01 11:58:40 +0300

    Warning added

--- src/components/Federation.jsx
+++ src/components/Federation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import images from '../images';
 
 export default function Federation(props) {
     return (
@@ -6,15 +7,20 @@ export default function Federation(props) {
             <div className="s-alert s-alert--primary">
                 <p className="Session__federations__alert">
                     <span>StellarTerm federation address</span>
-                    <span>
-                        <input type="submit" className="s-button Session__federations__button" value="Enable" />
-                    </span>
+                    <button className="s-button Session__federations__button">Enable</button>
                 </p>
             </div>
             <div className="Session__Account__text">
-                You can set an alias for your StellarTerm account. We’ll use this in our trollbox, and it will become
-                your payment alias, so people can send you money more easily.You can use this alias, including
-                the*stellarx.com, instead of your public key to receive payments on Stellar.
+                <p className="federation-warning">
+                    <img src={images.error} className="federation-icon" alt="Error" />
+                    <span className="federations-warning-text">This federation address is already in use. Please choose a different alias.</span>
+                </p>
+
+                <p>
+                    You can set an alias for your StellarTerm account. We’ll use this in our trollbox, and it will
+                    become your payment alias, so people can send you money more easily. You can use this alias,
+                    including the*stellarx.com, instead of your public key to receive payments on Stellar.
+                </p>
             </div>
         </div>
     );
